d16bccb084599b508ca19abc57ec6cca086a8a2a
lucas_ferrari <lucas.ferrari3007@gmail.com>
2024-12-11 17:03:58 -0300

    sfdf

--- app.py
+++ app.py
@@ -37,6 +37,17 @@ app = Flask(__name__)
 
 @app.route('/recomendar', methods=['POST'])
 def recomendar_remedio():
+    modelo_salvo2 = 'modelo_random_forest2.pkl'
+    vectorizer_salvo2 = 'vectorizer2.pkl'
+
+    try:
+        model2 = joblib.load(modelo_salvo2)
+        vectorizer2 = joblib.load(vectorizer_salvo2)
+        logging.info("Modelo e vetorizer carregados com sucesso.")
+    except FileNotFoundError:
+        logging.error("Modelo ou vetorizer não encontrados. Certifique-se de que foram treinados e salvos anteriormente.")
+
+
     # Recebendo dados do cliente
     dados = request.get_json()
     sintomas = dados.get('sintomas')
@@ -51,6 +62,25 @@ def recomendar_remedio():
     # Fazendo a previsão
     remedio = model.predict(sintomas_transf)[0]
     return jsonify({'remedio': remedio})
+
+
+@app.route('/chat', methods=['POST'])
+def recomendar_funcao():
+    # Recebendo dados do cliente
+    dados = request.get_json()
+    mensagem = dados.get('mensagem')
+
+    if not mensagem:
+        return jsonify({'error': 'Mensagem não fornecidos'}), 400
+
+    # Pré-processamento
+    mensagem_preproc = preprocessar_texto(mensagem)
+    mensagem_transf = vectorizer.transform([mensagem_preproc])
+
+    # Fazendo a previsão
+    funcao = model.predict(mensagem_transf)[0]
+    return jsonify({'funcao': funcao})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
